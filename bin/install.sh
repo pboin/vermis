b0d56663029b300bfa017bdeb177e4197e6d8f1c
pboin <paul.boin@gmail.com>
2014-09-02 22:34:46 -0400

    Cleanup

--- bin/install.sh
+++ bin/install.sh
@@ -5,15 +5,24 @@
 
 URL='http://downloads.raspberrypi.org/arch_latest'
 
-
+# check for enough space ( 200m for downloaded file + manipulation )
 
 # check for dependencies
 which wget;
 if [ $? -ne "0" ]; then echo "FAIL:  You need to install 'wget'."; fi;
 
 
-
+# get the unaltered file
 wget $URL
+
+
+# download it
+
+# inject the configuration file
+
+# inject the configuration script
+
+# repackage / recompress the image file
 
 
 
